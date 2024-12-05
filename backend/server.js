20f9ffa45a102c980338840467f4751007b9f4fa
xero <Ranzer09>
2024-12-05 13:12:58 +0530

    b

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,19 @@ const express = require('express')
 const mongoose = require('mongoose')
 const userRoutes= require('./routes/user')
 const productRoutes= require('./routes/products')
+const cors = require('cors');
 const cartRoutes= require('./routes/cart')
 require('dotenv').config()
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+const allowedOrigins = ['http://localhost:3000', 'https://zapzoom.onrender.com'];
+app.use(cors({
+    origin: allowedOrigins, // Restrict to allowed origins
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+    credentials: true, // Allow credentials (e.g., cookies, authorization headers)
+}));
 
 //connect to db
 mongoose.connect(process.env.MONGO_URI)
